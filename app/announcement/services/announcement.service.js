7454e527b0a710113e2a42ab897317a2b7003330
Gabriel Regalado <Gabriel Regalado>
2016-10-03 16:40:16 -0500

    prepare announcement

--- app/announcement/services/announcement.service.js
+++ app/announcement/services/announcement.service.js
@@ -1,8 +1,6 @@
 (function() {
 	'use strict';
-
 	angular.module('announcement').service('AnnouncementService', ['$q', '$http', 'API', AnnouncementService]);
-
 	function AnnouncementService($q, $http, API) {
 		return {
 			getAllUniversities: getAllUniversities,
@@ -12,9 +10,7 @@
 			addAnnouncement: addAnnouncement,
 			editAnnouncement: editAnnouncement,
 			deleteAnnouncement: deleteAnnouncement
-
 		};
-
 		function getAllUniversities() {
 			var universityDefer = $q.defer();
 			$http.get(API.university.list)
@@ -27,10 +23,8 @@
 			});
 			return universityDefer.promise;
 		}
-
 		function getAllAnnouncements() {
 			var announcementDefer = $q.defer();
-
 			$http.get(API.announcement.list)
 			.success(function(response) {
 				announcementDefer.resolve(response);
@@ -39,12 +33,10 @@
 				announcementDefer.reject(error);
 				console.log(error);
 			});
-
 			return announcementDefer.promise;
 		}
 		function getAnnouncementsAvailable() {
 			var announcementDefer = $q.defer();
-
 			$http.get(API.announcement.aviable)
 			.success(function(response) {
 				announcementDefer.resolve(response);
@@ -53,13 +45,10 @@
 				announcementDefer.reject(error);
 				console.log(error);
 			});
-
 			return announcementDefer.promise;
 		}
-
 		function getAnnouncementById(id) {
 			var announcementDefer = $q.defer();
-
 			$http.get(API.announcement.ById.replace(':id', id))
 			.success(function(response) {
 				announcementDefer.resolve(response);
@@ -68,13 +57,10 @@
 				announcementDefer.reject(error);
 				console.log(error);
 			});
-
 			return announcementDefer.promise;
 		}
-
 		function addAnnouncement(data) {
 			var announcementDefer = $q.defer();
-
 			$http.post(API.announcement.add, data)
 			.success(function(response) {
 				announcementDefer.resolve(response);
@@ -83,10 +69,8 @@
 				announcementDefer.reject(error);
 				console.log(error);
 			});
-
 			return announcementDefer.promise;
 		}
-
 		function editAnnouncement(data) {
 			var announcementDefer = $q.defer();
 			$http.put(API.user.add, data)
@@ -99,10 +83,8 @@
 			});
 			return announcementDefer.promise;
 		}
-
 		function deleteAnnouncement(id) {
 			var announcementDefer = $q.defer();
-
 			$http.post('http://www.stamina.dev/API/public/api/v1/usuario/', id)
 			.success(function(response) {
 				announcementDefer.resolve(response);
@@ -111,7 +93,6 @@
 				announcementDefer.reject(error);
 				console.log(error);
 			});
-
 			return announcementDefer.promise;
 		}
 	}
